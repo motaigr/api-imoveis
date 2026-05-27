af9f5bc31436f2202690212eb317a8b52440a2f7
motaigr <igor10.mota@hotmail.com>
2026-05-27 20:45:25 -0300

    alterações para o deploy

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI, HTTPException
-import httpx
 
 app = FastAPI()
 
 @app.get("/calcular")
-def calcular(bairro: str, area: float, quartos: int):
-    resposta = httpx.get("http://127.0.0.1:8000/valor-m2", params={"bairro": bairro})
-    if resposta.status_code != 200:
-        raise HTTPException(status_code=404, detail="Bairro não encontrado")
-    dados = resposta.json()
-    valor_m2 = dados["valor_m2"]
+def calcular(bairro: str, area: float, quartos: int, vagas: int):
+    resultado = calcular_valor_m2(bairro)
+    valor_m2 = resultado["valor_m2"]
     fator_quartos = 1.0 + (quartos - 2) * 0.05
-    preco = area * valor_m2 * fator_quartos
-    return {"bairro": bairro, "area": area, "quartos": quartos, "preco_sugerido": preco}
+    fator_vagas = 1.0 + vagas * 0.03
+    preco = area * valor_m2 * fator_quartos * fator_vagas
+    return {"bairro": bairro, "area": area, "quartos": quartos, "vagas": vagas, "preco_sugerido": preco}
 
 @app.get("/valor-m2")
 def calcular_valor_m2(bairro: str):
